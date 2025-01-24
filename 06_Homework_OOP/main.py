608e40e92075da0a3faf81306e1706f6779a1c52
Aernjdz <zarichniukandriy5@gmail.com>
2025-01-24 17:59:16 +0200

    Add Task 2

--- 06_Homework_OOP/main.py
+++ 06_Homework_OOP/main.py
@@ -1,3 +1,32 @@
+# Task 1
+class Temperature:
+    _conversion_count = 0  
+
+    @staticmethod
+    def celsius_to_fahrenheit(celsius):
+        """Convert Celsius to Fahrenheit."""
+        Temperature._conversion_count += 1
+        return (celsius * 9/5) + 32
+
+    @staticmethod
+    def fahrenheit_to_celsius(fahrenheit):
+        """Convert Fahrenheit to Celsius."""
+        Temperature._conversion_count += 1
+        return (fahrenheit - 32) * 5/9
+
+    @staticmethod
+    def get_conversion_count():
+        """Return the total number of conversions."""
+        return Temperature._conversion_count
+
+
+
+print(Temperature.celsius_to_fahrenheit(25)) 
+print(Temperature.fahrenheit_to_celsius(77))  
+print(Temperature.get_conversion_count())  
+
+
+#Task 2
 class Airplane:
     def __init__(self, model, passenger_capacity, current_passengers=0):
         self.model = model
